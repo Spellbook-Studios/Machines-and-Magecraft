51f022b8a05cda0f2e5884d89bde1cdff2963df4
Sebastian August Snoer <ssnoer@proton.me>
2024-07-26 16:56:41 +0200

    Fix!

--- packwiz-build/kubejs/startup_scripts/src/tanned_leather.js
+++ packwiz-build/kubejs/startup_scripts/src/tanned_leather.js
@@ -3,5 +3,5 @@
 // Visit the wiki for more info - https://kubejs.com/
 
 StartupEvents.registry('item', event => {
-    event.create("tanned_leather").displayName("Tanned Leather").texture("mm:item/tanned_leather").maxStackSize(64).tag("#forge:leather/tanned")
+    event.create("tanned_leather").displayName("Tanned Leather").texture("mm:item/tanned_leather").maxStackSize(64).tag("forge:leather/tanned")
 })
