ff440271e38f4b6ac6dcb93842308e4bf84e4aea
Sebastian August Snoer <ssnoer@proton.me>
2024-07-27 10:56:38 +0200

    Fixes for emi

--- 3. Worldgen & Exploration/kubejs/server_scripts/src/leather_recipes.js
+++ 3. Worldgen & Exploration/kubejs/server_scripts/src/leather_recipes.js
@@ -3,5 +3,20 @@
 // Visit the wiki for more info - https://kubejs.com/
 
 ServerEvents.recipes(event => {
-    
+    Item.of('kubejs:cured_leather', 0), // arg 1: output
+        [
+            'SSS',
+            'LLL', // arg 2: the shape (array of strings)
+            'SSS'
+        ],
+        {
+            S: 'mekanism:salt',
+            L: 'kubejs:untanned_leather'
+        }
 })
+
+LootJS.modifiers((event) => {
+    event
+        .addLootTypeModifier(LootType.ENTITY).matchLoot(Item.of("minecraft:leather")).replaceLoot("minecraft:leather", "kubejs:untanned_leather")
+});
+
