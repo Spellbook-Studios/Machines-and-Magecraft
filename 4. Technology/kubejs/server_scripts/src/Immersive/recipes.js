6acdef04cc676ace5d6b1b648eab078a683dd730
Sebastian August Snoer <ssnoer@proton.me>
2024-07-29 11:31:36 +0200

    Dust recipes

--- 4. Technology/kubejs/server_scripts/src/Immersive/recipes.js
+++ 4. Technology/kubejs/server_scripts/src/Immersive/recipes.js
@@ -1,5 +1,20 @@
 // priority: 0
 
+const disabled_immersive_generators = [
+    'immersiveengineering:watermill',
+    'immersiveengineering:waterwheel_segment',
+    'immersiveengineering:windmill_blade',
+    'immersiveengineering:windmill',
+    'immersiveengineering:windmill_sail',
+    'immersiveengineering:dynamo'
+]
+
+ServerEvents.tags('item', event => {
+    disabled_immersive_generators.forEach(item =>  {
+        event.add("c:hidden_from_recipe_viewers", item)
+    })
+})
+
 ServerEvents.recipes(event => {
     // Iron Mechanical Component
     event.remove({output: 'immersiveengineering:component_iron'})
@@ -36,6 +51,29 @@ ServerEvents.recipes(event => {
     // Make immersive funger med create
     event.replaceInput({ output: 'immersiveengineering:blastbrick'}, 'minecraft:brick', '#forge:plates/obsidian')
 
-    // Remove immersive waterwheel
-    event.remove({output: 'immersiveengineering:waterwill'})
+    // Remove immersive mills (create has an alternative, that can generate power)
+    disabled_immersive_generators.forEach(item =>  {
+        event.remove({output: item})
+    })
+
+    // Remove hammer based dust recipes
+    const dust_recipes = event.findRecipes({ input: 'immersiveengineering:hammer' })
+
+    dust_recipes.forEach(recipe => {
+        if(recipe.getOriginalRecipeResult().item.toString().contains("dust")) {
+            const ingredients = recipe.getOriginalRecipeIngredients();
+            var main_ingredient;
+            var out = recipe.getOriginalRecipeResult().item;
+
+            for(const ingredient of ingredients) {
+                if(!ingredient.itemStacks[0].item.toString().contains("hammer")) {
+                    main_ingredient = ingredient.itemStacks[0].item
+                }
+            }
+            if(main_ingredient) {
+                event.recipes.create.milling(Item.of(out), main_ingredient)
+                event.remove({ input: 'immersiveengineering:hammer', output: out })
+            }
+        }
+    })
 })
